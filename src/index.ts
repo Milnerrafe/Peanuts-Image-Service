74df27b6b388324a11d5e6492df41d83bd9f095b
Rafe Milner <milnerrafe@outlook.com>
2026-03-13 20:43:57 +0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ export default {
 	async fetch(request, env): Promise<Response> {
 		const url = new URL(request.url);
 		if (url.pathname.startsWith('/random')) {
-			let random = Math.floor(Math.random() * 10);
+			let random = Math.floor(Math.random() * 5);
 			let newurl = `https://peanuts.intelligencesystems.org/${random}.avif`;
 			let modifiedRequest = new Request(newurl, request);
 			console.log(modifiedRequest);
