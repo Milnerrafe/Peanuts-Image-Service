a5aa65f7fa35c217559a1e2e1cbb99c9eb5b8f00
Rafe Milner <milnerrafe@outlook.com>
2026-03-14 18:37:23 +0800

    Fixed zero error
    Math.random combined with Math.floor returned a number between 0 and 4. There is not a 0.avif file, therefore it caused 404 errors. Now, because we are adding 1 to it, it will always be between 1 and 5, also enabling up to 5 AVIFs to be used.

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ export default {
 	async fetch(request, env): Promise<Response> {
 		const url = new URL(request.url);
 		if (url.pathname.startsWith('/random')) {
-			let random = Math.floor(Math.random() * 5);
+			let random = Math.floor(Math.random() * 5) + 1;
 			let newurl = `https://peanuts.intelligencesystems.org/${random}.avif`;
 			let modifiedRequest = new Request(newurl, request);
 			console.log(modifiedRequest);
